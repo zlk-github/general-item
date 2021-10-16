0151c7380229713891a82b2e0ec51828c3666393
likuan.zhou <19921105zhou>
2021-10-16 17:49:43 +0800

    docs: 目录暂存

--- src/main/java/com/zlk/jdk/jdk8/stremTest.java
+++ src/main/java/com/zlk/jdk/jdk8/stremTest.java
@@ -9,11 +9,18 @@ package com.zlk.jdk.jdk8;
  */
 public class stremTest {
     public static void main(String[] args) {
+           //key string
+          //basicConfigDtoList.stream().collect(Collectors.toMap(BasicConfigDto::getCode, LocalFieldUtil::getFieldVal));
+          //key List<object>
           /* Map<String, List<BasicConfigDto>> basicTypeMap = basicConfigDtoList.stream().collect(Collectors.groupingBy(BasicConfigDto::getBasicTypeCode));
 
+         //key object
         Map<String, BasicConfigVo> dictMap = basicConfigVoList.stream().collect(Collectors.toMap(BasicConfigVo::getCode, dict -> dict));*/
         // LocalFieldUtil.getFieldVal()
        // Map<String, String> configVoMap = basicConfigVoList.stream().collect(Collectors.toMap(BasicConfigVo::getCode, BasicConfigVo::getArName));
+
+        // 获取部分字段转map
+       // List<WaybillReceiverDto> collect = list.stream().map(DispatchScanDto::getWaybillReceiver).collect(Collectors.toList());
 
     }
 }
