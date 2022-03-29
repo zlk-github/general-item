18aa9d4c3c5d2797f646bcb6b117f04b3f86ccfd
likuan.zhou <19921105zhou>
2022-03-29 09:20:38 +0800

    feat: threadLocal注释说明

--- src/main/java/com/zlk/jdk/thread/threadLocal/ThreadLocalTest.java
+++ src/main/java/com/zlk/jdk/thread/threadLocal/ThreadLocalTest.java
@@ -3,6 +3,14 @@ package com.zlk.jdk.thread.threadLocal;
 /**
  *  ThreadLocal:每个线程独有本地变量，解决多线程使用同一变量导致的线程安全问题。
  *      对每个线程维护一个ThreadLocalMap。ThreadLocal用于管理线程与ThreadLocalMap关系。
+ *      一个线程对应一个ThreadLocalMap，ThreadLocalMap内容是一个数组table（table[i] = new Entry(firstKey, firstValue)）
+ *      ThreadLocalMap(ThreadLocal<?> firstKey, Object firstValue) {
+ *             table = new Entry[INITIAL_CAPACITY];
+ *             int i = firstKey.threadLocalHashCode & (INITIAL_CAPACITY - 1);
+ *             table[i] = new Entry(firstKey, firstValue);
+ *             size = 1;
+ *             setThreshold(INITIAL_CAPACITY);
+ *         }
  *      数据应用：如多数据源切换。
  *
  * @author likuan.zhou
@@ -15,7 +23,7 @@ public class ThreadLocalTest {
         // 获取当前线程对应本地变量
         System.out.println("threadLocal = " + threadLocal.get());
         // 移除当前线程对应本地变量
-        threadLocal.remove();
+        //threadLocal.remove();
     }
 
     public static void main(String[] args) {
@@ -24,6 +32,7 @@ public class ThreadLocalTest {
             public void run() {
                 // 线程1中设置本地变量
                 threadLocal.set("threadLocal1");
+                threadLocal.set("threadLocal3");
                 // 调用打印，并移除
                 print("threadLocal1");
                 System.out.println("（threadLocal1）ThreadLocal after remove:"+threadLocal.get());
